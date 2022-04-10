fcd3e7341eb3459adf744c3b3a97e9d69868ec24
priyanshu613 <96369866+priyanshu613@users.noreply.github.com>
2022-04-10 17:46:24 +0530

    Add files via upload

---
+++ First_Java.java
@@ -0,0 +1,7 @@
+package Unit_01;
+
+public class First {
+	public static void main(String[] args) {
+		System.out.println("Hello buddy");
+	}
+}
---
+++ P2_Task01_KeywordsInJava.java
@@ -0,0 +1,27 @@
+package Unit_01;
+
+public class P2_Task01_KeywordsInJava {
+public static void main(String[] args)
+{
+	
+	//variable name
+	//Syntax error on token "double", invalid VariableDeclaratorId
+	int double = 12;
+	
+	//function name
+	int new() {
+		//sysout[ctrl+space]
+		System.out.println();
+		
+		return 0;
+	}
+	
+	
+	
+	//class name [given below]
+	
+	//object
+	//object name can never be any keywords in java
+	ABC int = new ABC();
+}
+}
